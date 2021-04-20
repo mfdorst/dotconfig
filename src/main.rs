27e870261baa92d813ad3ab0941e5849015e98d1
Michael Dorst <m@mdorst.net>
2021-04-20 00:44:05 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{bail, Result};
 use clap::{load_yaml, App, ArgMatches};
 use serde::Deserialize;
+use std::ffi::OsStr;
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
@@ -19,7 +20,7 @@ fn main() -> Result<()> {
     // Symlink each file listed in config.links
     for Link { src, dest } in config.links {
         if let Err(e) = link(&src, &dest, &dotfiles_dir) {
-            println!("{}", link_error_msg(e)?);
+            println!("{}", e);
         }
     }
     Ok(())
@@ -31,34 +32,9 @@ macro_rules! link_error {
 
 fn link(src: &str, dest: &str, dotfiles_dir: &PathBuf) -> Result<()> {
     let src = get_src_path(dotfiles_dir, src)?;
-    let dest = get_dest_path(dest)?;
-    print!("Linking {} -> {}...", src.display(), dest.display());
-    match unix::fs::symlink(&src, &dest) {
-        Ok(()) => Ok(println!("done.")),
-        Err(_) => Err(link_error!(
-            "\nFailed to symlink {} -> {}. Skipping...",
-            src.display(),
-            dest.display()
-        )),
-    }
-}
-
-fn get_src_path(dotfiles_dir: &PathBuf, src: &str) -> Result<PathBuf> {
-    let src = dotfiles_dir.join(src);
-    let src = fs::canonicalize(&src)
-        .map_err(|_| link_error!("Path '{}' does not exist. Skipping...", src.display()))?;
-    Ok(src)
-}
-
-fn get_dest_path(dest: &str) -> Result<PathBuf> {
     let dest = expand_to_path_buf(&dest)?;
 
-    // Path::parent() returns None if the path is '/'.
-    let dest_parent = dest.parent().ok_or(link_error!(
-        "Cannot link to '{}'. Skipping...",
-        dest.display()
-    ))?;
-    let dest_parent = fs::canonicalize(&dest_parent).map_err(|_| {
+    let dest_parent = get_parent_dir(&dest).map_err(|_| {
         link_error!(
             "Cannot link to '{}' because its parent directory does not exist. Skipping...",
             dest.display()
@@ -68,27 +44,60 @@ fn get_dest_path(dest: &str) -> Result<PathBuf> {
         "Invalid destination path '{}'. Skipping...",
         dest.display()
     ))?;
+
+    backup_if_exists(&dest_parent, dest_file_name)?;
+
     let dest = dest_parent.join(dest_file_name);
 
-    if dest.exists() {
-        let mut backup_file = dest_file_name.to_owned();
+    print!("Linking {} -> {}...", src.display(), dest.display());
+    unix::fs::symlink(&src, &dest)
+        .map(|_| println!("done."))
+        .map_err(|e| {
+            link_error!(
+                "\nFailed to symlink {} -> {}. {}. Skipping...",
+                src.display(),
+                dest.display(),
+                e
+            )
+        })
+}
+
+fn get_src_path(dotfiles_dir: &PathBuf, src: &str) -> Result<PathBuf> {
+    let src = dotfiles_dir.join(src);
+    let src = fs::canonicalize(&src)
+        .map_err(|_| link_error!("Path '{}' does not exist. Skipping...", src.display()))?;
+    Ok(src)
+}
+
+fn backup_if_exists(parent_dir: &PathBuf, file_name: &OsStr) -> Result<()> {
+    let path = parent_dir.join(file_name);
+    if path.exists() {
+        let mut backup_file = file_name.to_owned();
         backup_file.push(
             chrono::Local::now()
                 .format("-backup-%Y-%m-%d-%H-%M-%S")
                 .to_string(),
         );
-        let backup = dest_parent.join(backup_file);
+        let backup = parent_dir.join(backup_file);
         print!(
             "The path '{}' already exists. Backing up to '{}'...",
-            dest.display(),
+            path.display(),
             backup.display()
         );
-        match fs::rename(&dest, backup) {
-            Ok(()) => println!("done."),
-            Err(_) => println!("\nBackup failed. Skipping..."),
-        }
+        fs::rename(&path, backup)
+            .map(|_| println!("done."))
+            .map_err(|e| link_error!("\nBackup faile. {}", e))
+    } else {
+        Ok(())
     }
-    Ok(dest)
+}
+
+fn get_parent_dir(path: &PathBuf) -> Result<PathBuf> {
+    if let Some(parent_dir) = path.parent() {
+        fs::canonicalize(&parent_dir).map_err(|e| e.into())
+    } else {
+        bail!("Cannot get parent of '{}'.", path.display())
+    }
 }
 
 fn get_config(args: &ArgMatches) -> Result<(Config, PathBuf)> {
@@ -111,15 +120,6 @@ fn get_config(args: &ArgMatches) -> Result<(Config, PathBuf)> {
 
 fn expand_to_path_buf(path: &str) -> Result<PathBuf> {
     Ok(shellexpand::full(path)?.to_string().into())
-}
-
-fn link_error_msg(error: anyhow::Error) -> Result<String> {
-    for cause in error.chain() {
-        if let Some(Error::LinkError(explanation)) = cause.downcast_ref::<Error>() {
-            return Ok(explanation.clone());
-        }
-    }
-    Err(error)
 }
 
 #[derive(Deserialize, Debug)]
